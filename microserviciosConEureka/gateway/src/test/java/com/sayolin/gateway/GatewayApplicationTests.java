de5f0b49fc600f8e0b871617de1adcd9a24ffad0
AlonsodeOjeda <141436151+AlonsodeOjeda@users.noreply.github.com>
2025-03-03 18:00:55 -0600

    Add files via upload

---
+++ microserviciosConEureka/gateway/src/test/java/com/sayolin/gateway/GatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sayolin.gateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GatewayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
